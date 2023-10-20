e4b7442250a158ebd15dc196c6b2306df2975645
Diego Bracellos <bracellos@gmail.com>
2023-10-19 21:48:38 -0300

    exercicios

--- progassinc/exercicios.js
+++ progassinc/exercicios.js
@@ -52,7 +52,8 @@ ex 1985 - 38 anos
 
 const confereInfo = (ano, idade) => {
     return new Promise((resolve, reject) => {
-        let calculo = 2023 - ano;
+        let date = new Date()
+        let calculo = date.getFullYear() - ano;
         if(calculo == idade){
             resolve("Correto")
         }else{
